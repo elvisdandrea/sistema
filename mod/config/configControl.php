b901e540bb1e7377fcce08734d1b005288df96ec
Elvis D'Andrea <elvis@gravi.com.br>
2015-10-03 18:47:15 -0300

    API config

--- mod/config/configControl.php
+++ mod/config/configControl.php
@@ -12,4 +12,15 @@ class configControl extends Control {
 
     }
 
+
+    public function getConfig() {
+
+        $result = $this->model()->getConfig($this->getId());
+
+        return RestServer::response(array(
+            'status'    => 200,
+            'config'    => $this->model()->getRows()
+        ));
+
+    }
 }
--- mod/config/configModel.php
+++ mod/config/configModel.php
@@ -8,4 +8,27 @@ class configModel extends Model {
     }
 
 
+    public function getConfig($station_id = 0) {
+
+        $this->addField('id');
+        $this->addField('station_id');
+        $this->addField('iso');
+        $this->addField('payment_account');
+        $this->addField('token');
+        $this->addField('sender_name');
+        $this->addField('sender_email');
+        $this->addField('area_code');
+        $this->addField('sender_phone');
+
+        $this->addFrom('config');
+
+        if ($station_id > 0)
+            $this->addWhere('station_id = "' . $station_id . '"');
+
+        $this->runQuery();
+
+        return !$this->isEmpty();
+
+    }
+
 }
